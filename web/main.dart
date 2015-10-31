44c5dce7748f352100bbfa08e1bb0d6b085a3422
Juan Alfredo Salas Santillana <juan.salas.santillana@ucsp.edu.pe>
2015-10-31 13:45:25 -0500

    Testing dart

---
+++ web/main.dart
@@ -0,0 +1,9 @@
+import 'dart:html';
+import 'dart:math' show Random;
+import 'dart:convert' show JSON;
+import 'dart:async' show Future;
+
+
+void main() {
+	window.console.debug('This will be logged to the console in the browser.');
+}
