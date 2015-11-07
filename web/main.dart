2f28e2c72bf87862acb769406e075bd6ff1f4181
Adolfo Tamayo <adolfo@inka-labs.com>
2015-11-07 08:46:39 -0500

    Dart test using Webservices, classes and DOM manipulation

--- web/main.dart
+++ web/main.dart
@@ -3,7 +3,46 @@ import 'dart:math' show Random;
 import 'dart:convert' show JSON;
 import 'dart:async' show Future;
 
+// https://www.mockable.io/a/#/space/demo7492033/rest
+
+class UserResource {
+    String path = "http://demo7492033.mockable.io/users/";
+    List user_list;
+    UserResource(callback){
+        HttpRequest.getString(path)
+            .then((String fileContents) {
+                this.user_list = JSON.decode(fileContents);
+                print("Users Loaded");
+                callback();
+            })
+            .catchError((Error error) {
+                print(error.toString());
+            });
+    }
+
+    getUser(String name){
+        for(var i=0;i<user_list.length; i++){
+            if(user_list[i]["name"] == name){
+                return user_list[i];
+            }
+        }
+        return {
+            "error": "User not found"
+        };
+    }
+
+}
+var userResource;
+
+onUsersReady(){
+    print(userResource);
+    print(userResource.user_list);
+    var user = userResource.getUser("Jorge");
+    print(user);
+    querySelector('#userName').text = "Hi ${user['name']}! Your email is ${user['email']}";
+}
 
 void main() {
-	window.console.debug('This will be logged to the console in the browser.');
+    userResource = new UserResource(onUsersReady);
 }
+
